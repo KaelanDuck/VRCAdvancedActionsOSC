30c8a1188197ca9af8312866e3c1087870079f4d
KaelanDuck <kaelan.duck@gmail.com>
2022-09-08 20:54:30 +1000

    Add example configs to build

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from cx_Freeze import setup, Executable
 # "packages": ["os"] is used as example only
 packages = ["os", "json", "traceback", "openvr", "sys", "time", "ctypes", "argparse", "pythonosc", "yaml", "glm"]
 exclude = ["tkinter", "asyncio", "concurrent", "html", "http", "lib2to3", "multiprocessing", "test", "unittest", "xmlrpc"]
-file_include = ["config.yaml", "Run Debug Mode.bat", "openvr/", "bindings/"]
+file_include = ["config.yaml", "Run Debug Mode.bat", "openvr/", "bindings/", "example_configs/"]
 bin_excludes = ["_bz2.pyd", "_decimal.pyd", "_hashlib.pyd", "_lzma.pyd", "_queue.pyd", "_ssl.pyd", "libcrypto-1_1.dll", "libssl-1_1.dll", "ucrtbase.dll", "VCRUNTIME140.dll"]
 
 build_exe_options = {"packages": packages, "excludes": exclude, "include_files": file_include, "bin_excludes": bin_excludes}
